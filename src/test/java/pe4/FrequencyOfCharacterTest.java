6c51d93fd443406c41ab01ea2c9f3f1fd3509e87
Pratyush Prakash <45649947+pratyushpp@users.noreply.github.com>
2018-12-31 17:50:15 +0530

    Update FrequencyOfCharacterTest.java

--- src/test/java/pe4/FrequencyOfCharacterTest.java
+++ src/test/java/pe4/FrequencyOfCharacterTest.java
@@ -1,16 +1,25 @@
 package pe4;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class FrequencyOfCharacterTest {
-    public void frequencyOfaCharacterFinder(String s)
-    {
-        String str="";
-        System.out.println("Length Of String:" + str.length());
-        System.out.println("Length Of String Without a :" + str.replace("a", "").length());
-        int charcount = str.length() - str.replaceAll("a", "").length();
-        System.out.println("Occurrence Of A Char In String: " + charcount);
-        //System.out.println();
-    }
+public class FrequencyOfCharacterTest
+{
+    FrequencyOfCharacter frequencyOfCharacter=new FrequencyOfCharacter();
 
+
+@Test
+    public void test1()
+{
+    assertEquals("occurence of letter a in a string", 4, frequencyOfCharacter.frequencyOfaCharacterFinder("javaaa"));
+
+}
+
+    @Test
+    public void test2()
+    {
+        assertEquals("occurence of letter a in a string", 10, frequencyOfCharacter.frequencyOfaCharacterFinder("Java is java again java again"));
+
+    }
 }
